9df0959650a72423ad969b0f556aab3851f56ee4
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-07-27 00:14:25 +0700

    feat: added xdg-desktop-portal-hyprland and polkit for hyprland and set default start command hyprland

--- src/apps/window-manager/hyprland/default.nix
+++ src/apps/window-manager/hyprland/default.nix
@@ -2,8 +2,20 @@
   environment.systemPackages = with pkgs;[
     hyprland
     waybar
+    xdg-desktop-portal-hyprland
+    polkit
   ];
   programs.hyprland.enable = false;
   programs.hyprland.xwayland.enable = true;
   programs.waybar.enable = true;
+
+  services.xserver.displayManager.session = [
+    {
+      manage = "window";
+      name = "hyprland";
+      start = ''
+        ${pkgs.hyprland}/bin/Hyprland
+      '';
+    }
+  ];
 }
