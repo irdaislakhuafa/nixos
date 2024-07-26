260728f02f815ee4afa15b074b7cec7baf467db6
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-07-27 00:15:01 +0700

    feat: enable sound and pulse audio

--- src/systems/hardware/default.nix
+++ src/systems/hardware/default.nix
@@ -2,4 +2,6 @@
   # enable hardware like wlan and etc
   hardware.enableRedistributableFirmware = true;
   hardware.opengl.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 }
