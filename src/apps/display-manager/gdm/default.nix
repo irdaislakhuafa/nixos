6f36493d2cbe12fbb676a074480701f54d925cf7
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-07-27 00:13:43 +0700

    feat: enable debug for gdm and set default session for gdm is `none+hyprland`

--- src/apps/display-manager/gdm/default.nix
+++ src/apps/display-manager/gdm/default.nix
@@ -5,4 +5,6 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.debug = true;
+  services.xserver.displayManager.defaultSession = "none+hyprland";
 }
