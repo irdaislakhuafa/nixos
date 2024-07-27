4300787a207a6174cda09bb80fdc2803d2b4247f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-07-27 19:30:04 +0700

    feat: rename sound.enable to xdg-sounds.enable

--- src/systems/hardware/default.nix
+++ src/systems/hardware/default.nix
@@ -2,6 +2,6 @@
   # enable hardware like wlan and etc
   hardware.enableRedistributableFirmware = true;
   hardware.opengl.enable = true;
-  sound.enable = true;
+  xdg.sounds.enable = true;
   hardware.pulseaudio.enable = true;
 }
