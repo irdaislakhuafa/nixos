a247f7d8997e2241582792136636c1af7a12f6c3
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-07-28 01:31:18 +0700

    use pipewire instead of pulse audio

--- src/systems/hardware/default.nix
+++ src/systems/hardware/default.nix
@@ -1,7 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   # enable hardware like wlan and etc
   hardware.enableRedistributableFirmware = true;
   hardware.graphics.enable = true;
   xdg.sounds.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.enable = false;
+  environment.systemPackages = with pkgs; [ pavucontrol ];
+  services.pipewire.enable = true;
+  services.pipewire.audio.enable = true;
+  services.pipewire.wireplumber.enable = true;
+  services.pipewire.alsa.enable = true;
+  services.pipewire.alsa.support32Bit = true;
+  services.pipewire.jack.enable = true;
+  services.pipewire.pulse.enable = true;
 }
