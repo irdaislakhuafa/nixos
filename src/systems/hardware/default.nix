24d712033a6e0e82ebb081b2b26f906e75d62b20
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-07-27 20:06:21 +0700

    feat: rename hardware.opengl.enable to hardware.graphics.enable

--- src/systems/hardware/default.nix
+++ src/systems/hardware/default.nix
@@ -1,7 +1,7 @@
 { ... }: {
   # enable hardware like wlan and etc
   hardware.enableRedistributableFirmware = true;
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
   xdg.sounds.enable = true;
   hardware.pulseaudio.enable = true;
 }
