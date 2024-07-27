c5ce72cfedcab86f14cf292c255c6e506d34f8db
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-07-27 13:34:10 +0700

    feat: disable gdm

--- src/apps/display-manager/gdm/default.nix
+++ src/apps/display-manager/gdm/default.nix
@@ -2,8 +2,8 @@
   environment.systemPackages = with pkgs;[
     gnome.gdm
   ];
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.displayManager.gdm.debug = true;
   services.xserver.displayManager.defaultSession = "none+hyprland";
