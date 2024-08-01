aee4c9f6c3e9b5dc3a9781ec3f77a7d730e7220b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-02 01:53:30 +0700

    feat(udisks): allow users group to format removable disks
    This change adds a Polkit rule to allow users in the 'users' group to format removable disks.
    
    This fixes an issue where users were not able to format removable disks without root access.

--- src/systems/disks/default.nix
+++ src/systems/disks/default.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }:
-{
+{ pkgs, config, ... }: {
   environment.systemPackages = with pkgs; [
     udisks
   ];
   services.udisks2.enable = true;
   services.udisks2.mountOnMedia = false;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id.startsWith("org.freedesktop.udisks2.") && subject.isInGroup("${config.users.users.i.group}")) {
+        return polkit.Result.YES;
+      }
+    })
+  '';
 }
