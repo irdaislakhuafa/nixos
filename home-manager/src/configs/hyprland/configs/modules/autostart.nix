f8ac08fe95654d4e79cb5200771c1e90e4c081e0
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-05 18:56:48 +0700

    fix(hyprland): remove deprecated swaybg process kill
    This commit removes the deprecated swaybg process kill from the autostart module.
    
    BREAKING CHANGE: The swaybg process will no longer be killed on startup. This may cause issues if swaybg is not already running.

--- home-manager/src/configs/hyprland/configs/modules/autostart.nix
+++ home-manager/src/configs/hyprland/configs/modules/autostart.nix
@@ -1,6 +1,4 @@
 { config, pkgs, ... }: ''
   exec = ${pkgs.psmisc}/bin/killall ${pkgs.swaybg}/bin/hyprnotify
   exec = ${pkgs.hyprnotify}/bin/hyprnotify
-  # exec = ${pkgs.psmisc}/bin/killall ${pkgs.swaybg}/bin/swaybg
-  # exec = ${pkgs.swaybg}/bin/swaybg -i $HOME/.config/hypr/wallpaper.png 
 ''
