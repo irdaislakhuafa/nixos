0085b0f27c227770791784637fdfef00a726bfba
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-09 01:38:43 +0700

    feat(desktop): add xdg-desktop-portal support for hyprland
    This commit adds support for the xdg-desktop-portal-hyprland package, which provides a portal implementation for the hyprland window manager.
    
    This allows applications to use the xdg-desktop-portal API to interact with the desktop environment, such as opening files and directories, and launching applications.

--- src/systems/desktop/default.nix
+++ src/systems/desktop/default.nix
@@ -2,7 +2,13 @@
   environment.systemPackages = with pkgs; [
     libnotify
     notify-osd
+    rtkit
+    xdg-utils
   ];
   xdg.portal.xdgOpenUsePortal = true;
   xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    pkgs.xdg-desktop-portal-hyprland
+  ];
+  security.rtkit.enable = true;
 }
