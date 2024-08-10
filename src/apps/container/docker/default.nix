112de0f904e8a24521407aa7b5f05578c0cde9fb
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-10 13:23:47 +0700

    feat: added remove docker image/containers aliases
    This commit adds several new aliases to easily remove docker images and containers.
    
    BREAKING CHANGE: The `data-root` setting for the docker daemon has been moved from `/media/Docker` to `$HOME/.config/docker`.

--- src/apps/container/docker/default.nix
+++ src/apps/container/docker/default.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
+let
+  dataRootDir = "/media/Docker";
+in
 {
   environment.systemPackages = with pkgs; [
     docker
@@ -13,14 +16,54 @@
   virtualisation.docker.enableOnBoot = false;
   virtualisation.docker.autoPrune.enable = false;
   virtualisation.docker.daemon.settings = {
-    "data-root" = "/media/Docker";
+    "data-root" = dataRootDir;
   };
 
   # docker rootless
   virtualisation.docker.rootless.enable = true;
   virtualisation.docker.rootless.daemon.settings = {
-    "data-root" = "/media/Docker";
+    "data-root" = dataRootDir;
   };
 
   services.dockerRegistry.enable = true;
+  environment.shellAliases = (
+    let
+      delimiter = "|";
+      strFmtImgs = "{{.ID}} | {{.Size}}\t| {{.Repository}}:{{.Tag}}";
+      shell = "sh";
+    in
+    {
+      dormiop = ''
+        listID="$(docker images --format="${strFmtImgs}" | fzf -m | cut -d '|' -f 1)";
+        if ! [ $listID = "" ]; then
+          ${shell} -c "docker rmi $(echo $listID) && notify-send 'Success remove docker images'" & ;
+        fi;
+      '';
+      dormop = ''
+        listID="$(docker ps -a | fzf -m | cut -d ' ' -f 1)"; 
+        if ! [ $listID = "" ]; then
+          ${shell} -c "docker rm $(echo $listID) && notify-send 'Success remove docker containers'" & ;
+        fi;
+      '';
+      dormops = ''
+        listID="$(docker ps -a --size | fzf -m | cut -d ' ' -f 1)"; 
+        if ! [ $listID = "" ]; then
+          ${shell} -c "docker rm $(echo $listID) && notify-send 'Success remove docker containers'" & ;
+        fi;
+      '';
+      doexecit = ''docker exec -it $(docker ps | fzf | cut -d " " -f 1)'';
+      dostart = ''
+        listID="$(docker ps -a | fzf -m | cut -d ' ' -f 1)"; 
+        if ! [ $listID = "" ]; then
+          ${shell} -c "docker start $(echo $listID) && notify-send 'Starting docker container done'" & ;
+        fi;
+      '';
+      dostop = ''
+        listID="$(docker ps | fzf -m | cut -d ' ' -f 1)"; 
+        if ! [ $listID = "" ]; then
+          ${shell} -c "docker stop $(echo $listID) && notify-send 'Stoping docker container done'" & ;
+        fi;
+      '';
+    }
+  );
 }
