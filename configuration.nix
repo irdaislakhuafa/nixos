b3b8ef71c295fdb2ec79d75e9567df6f9b882d56
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-10 13:24:20 +0700

    refactor: adjust configuration for apps and systems imports
    Moves the system and app imports to their own file.
    
    Adds a missing comment.

--- configuration.nix
+++ configuration.nix
@@ -2,24 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
-{
-  imports =
-    [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./src/systems/default.nix
-      ./src/apps/default.nix
-    ];
-
-  # networking.hostName = "developer"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+{ config, lib, pkgs, ... }: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./src/systems/default.nix
+    ./src/apps/default.nix
+  ];
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
@@ -29,29 +18,14 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     home-manager
   ];
-
-  # enable i3
-  # services.xserver.windowManager.i3.enable = true;
-
-  # enable gdm
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
