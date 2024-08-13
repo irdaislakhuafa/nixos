32b1e8c18215046d2cffa3d3b7378557dbc0f5dc
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-14 01:36:20 +0700

    refactor: remove unused packages
    This commit removes the following unused packages:
    - vscode
    - neovim
    - systemctl-tui
    
    These packages were no longer needed and were causing unnecessary bloat in the system.

--- src/apps/ide/default.nix
+++
@@ -1,6 +0,0 @@
-{ ... }: {
-  imports = [
-    ./vscode/default.nix
-    ./neovim/default.nix
-  ];
-}
--- src/systems/systemd/default.nix
+++ src/systems/systemd/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs;[
-    systemctl-tui
+    sysz
   ];
 }
