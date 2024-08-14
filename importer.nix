67b44d0929d42c3e81a8aae4058a1896bc52395f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-15 03:48:47 +0700

    refactor: remove unused nix expression
    Removes an unused nix expression that was introducing a bug in some cases.

--- importer.nix
+++
@@ -1,5 +0,0 @@
-{ cwd, ... }:
-let
-  dirs = builtins.filter (f: f != "default.nix") (builtins.attrNames (builtins.readDir cwd));
-in
-builtins.map (dir: "${builtins.toPath ./${dir}}" + "/default.nix") dirs
