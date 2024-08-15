3297dcbe8772f12fbe158a091790f8337bd3357c
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-15 10:35:08 +0700

    feat: provide fzf integration for gouts and gbrd shell aliases
    Provides fzf integration for the gouts and gbrd shell aliases, making it easier to select branches to checkout or delete.

--- src/apps/cli/git/default.nix
+++ src/apps/cli/git/default.nix
@@ -8,11 +8,22 @@
   ];
 
   programs.git.enable = true;
-  environment.shellAliases = {
-    gst = "git status";
-    gad = "git add";
-    gps = "git push";
-    grph = "";
-    gpl = "git pull";
-  };
+  environment.shellAliases =
+    let
+      commandToBranch = { cmd, ... }: ''
+        branch=$(git branch --all | fzf | cut -d ' ' -f 3);
+        if ! [ "$branch" = "" ]; then
+          ${cmd};
+        fi;
+      '';
+    in
+    {
+      gst = "git status";
+      gad = "git add";
+      gps = "git push";
+      grph = "";
+      gpl = "git pull";
+      gouts = commandToBranch { cmd = "git checkout $branch"; };
+      gbrd = commandToBranch { cmd = "git branch -D $branch"; };
+    };
 }
