bd51b76e2a9d8e5f5c1f17b945f647b5300dc1f1
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-16 01:36:20 +0700

    feat(video-player): add ffmpeg-qsv support
    Adds qsv hardware acceleration support for ffmpeg.
    
    BREAKING CHANGE: ffmpeg package is now replaced with ffmpeg-full. This may require recompiling any packages that depend on ffmpeg.

--- src/apps/video-player/default.nix
+++ src/apps/video-player/default.nix
@@ -2,7 +2,21 @@
   environment.systemPackages = with pkgs; [
     mpv
     mediainfo
-    ffmpeg
+    ffmpeg-full
     ytfzf
   ];
+  environment.shellInit = ''
+    function ff_convert_segments_qsv() {
+      input_file="$1";
+      [ "$input_file" = "" ] && echo -n "source path: " && read -n input_file ;
+
+      seg_time="$2";
+      [ "$seg_time" = "" ] && echo -n "segments time: " && read -n seg_time ;
+
+      prefix="$3";
+      [ "$prefix" = "" ] && echo -n "prefix: " && read -n prefix ;
+      
+      ffmpeg -loglevel quiet -i "$input_file" -c:v h264_qsv -c:a aac -map 0 -segment_time $seg_time -f segment -reset_timestamps 1 "$(echo $prefix)%03d.mp4"
+    }
+  '';
 }
