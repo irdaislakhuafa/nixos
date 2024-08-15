f7da9638208efd5cffb853cd3e1218f197cce2b5
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-15 18:34:20 +0700

    refactor: simplify importer
    Use builtins.readDir instead of builtins.baseNameOf to get directory names.

--- importer.nix
+++ importer.nix
@@ -1,8 +1,7 @@
 { dir, ... }: (
   let
-    cwd = builtins.baseNameOf dir;
     dirs = builtins.filter (f: f != "default.nix") (builtins.attrNames (builtins.readDir dir));
   in
-  builtins.map (d: "${builtins.toPath "${dir}/${d}"}" + "/default.nix") dirs
+  builtins.map (d: "${builtins.toPath "${dir}/${d}"}/default.nix") dirs
 )
 
