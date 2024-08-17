7b5bcc1b6cfb527ea40d0e4fb1c744f36d437bed
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-08-17 21:18:49 +0700

    refactor(hyprland): remove old neovim config
    Remove the old neovim config from home-manager.
    
    BREAKING CHANGE: The neovim config is no longer managed by home-manager.

--- home-manager/src/configs/neovim/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, ... }: {
-  programs.neovim = {
-    defaultEditor = true;
-    enable = true;
-    vimAlias = true;
-    viAlias = true;
-    plugins = with pkgs.vimPlugins; [ ];
-  };
-}
