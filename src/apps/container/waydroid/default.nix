66969d0009b23ad8e8317d2a2f384c6666b7471e
irda islakhu afa <irdhaislakhuafa@gmail.com>
2024-10-10 03:15:12 +0700

    feat(waydroid): enable auto start
    This commit enables auto start for waydroid.
    
    The previous commit had
    `virtualisation.waydroid.enable = false`
    
    This commit enables the virtualisation by changing it to
    `virtualisation.waydroid.enable = true`

--- src/apps/container/waydroid/default.nix
+++ src/apps/container/waydroid/default.nix
@@ -1,4 +1,12 @@
-{ ... }: {
+{ pkgs, lib, ... }:
+let
+  isEnableAutoStart = false;
+in
+{
+  # recommended to use VANILLA image and install GAPPS from https://github.com/irdaislakhuafa/waydroid_script.git (forked)
   # images stored in /var/lib/waydroid/images
-  virtualisation.waydroid.enable = false;
+  environment.systemPackages = with pkgs;[
+    waydroid
+  ];
+  virtualisation.waydroid.enable = isEnableAutoStart;
 }
