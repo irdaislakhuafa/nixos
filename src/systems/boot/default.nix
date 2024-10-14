b02c8daa675f7bb9fb33ccaef2ce6fa217204fed
irda islakhu afa <irdhaislakhuafa@gmail.com>
2024-10-15 01:21:12 +0700

    feat(boot): add xanmod and kvm
    Adds xanmod and kvm to the default boot configuration.

--- src/systems/boot/default.nix
+++ src/systems/boot/default.nix
@@ -2,4 +2,6 @@
   # Use the systemd-boot EFI boot loader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelModules = [ "kvm-linux" ];
 }
