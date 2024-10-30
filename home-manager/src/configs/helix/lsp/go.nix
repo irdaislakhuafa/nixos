594f259289585dd54a0265cbdde94b40d9ccbcc9
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2024-10-30 16:35:26 +0700

    feat(helix): add gopls and enable go support (#4)
    This commit adds support for the Go programming language to Helix by installing the gopls language server and necessary configurations.
    
    Additionally, the default Helix theme has been updated to "dark_plus" for improved readability.

---
+++ home-manager/src/configs/helix/lsp/go.nix
@@ -0,0 +1,45 @@
+{ ... }: {
+  language = {
+    name = "go";
+    auto-format = true;
+    roots = [ "go.mod" "go.sum" "go.work" ];
+    comment-token = "//";
+    block-comment-tokens = {
+      start = "/*";
+      end = "*/";
+    };
+    language-servers = [ "gopls" ];
+    indent = {
+      tab-width = 2;
+      unit = " ";
+    };
+  };
+  server = {
+    command = "gopls";
+    config = {
+      gofumpt = true;
+      local = "goimports";
+      semanticTokens = true;
+      staticcheck = true;
+      verboseOutput = true;
+      analyses = {
+        fieldalignment = true;
+        nilness = true;
+        unusedparams = true;
+        unusedwrite = true;
+        useany = true;
+      };
+      usePlaceholders = true;
+      completeUnimported = true;
+      hints = {
+        assignVariableType = true;
+        compositeLiteralFields = true;
+        compositeLiteralTypes = true;
+        constantValues = true;
+        functionTypeParameters = true;
+        parameterNames = true;
+        rangeVariableTypes = true;
+      };
+    };
+  };
+}
