0058ec20a44c627eccd980c222bd2cb8b3aefd11
irda islakhu afa <irdhaislakhuafa@gmail.com>
2024-12-10 23:57:15 +0700

    chore(doas): add symlink from doas to sudo

--- src/systems/security/doas/default.nix
+++ src/systems/security/doas/default.nix
@@ -2,6 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     doas
+    (runCommand "doas-sudo-symlink" {} '' mkdir -pv $out/bin/ && ln -sfv ${pkgs.doas}/bin/doas $out/bin/sudo'')
   ];
 
   security.doas.enable = true;
