c096d5396ab29fbf16680a25d1ee7a63317b48a3
irda islakhu afa <irdhaislakhuafa@gmail.com>
2024-12-28 02:03:00 +0700

    chore(deps): update home-manager to 35b98d2

--- flake.nix
+++ flake.nix
@@ -3,17 +3,41 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/d70bd19e0a38ad4790d3913bf08fcbfc9eeca507";
+    home-manager.url = "github:nix-community/home-manager/35b98d20ca8f4ca1f6a2c30b8a2c8bb305a36d84";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in
     {
       nixosConfigurations = {
         developer = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+          system = system;
           modules = [
             ./hardware-configuration.nix
             ./configuration.nix
+            home-manager.nixosModules.home-manager
+          ];
+        };
+      };
+
+      homeConfigurations = {
+        i = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = system;
+            config.allowUnfree = true;
+          };
+          modules = [
+            ./home-manager/home.nix
+            {
+              home = rec {
+                username = "i";
+                homeDirectory = "/home/${username}";
+                stateVersion = "25.05";
+              };
+            }
           ];
         };
       };
