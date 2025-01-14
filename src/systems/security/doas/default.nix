3a7986c8bca2e6bfd25b4ce6c0e58187b23f8bb1
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2025-01-15 01:30:29 +0700

    refactor(nix): update nix configurations (#19)

--- src/systems/security/doas/default.nix
+++ src/systems/security/doas/default.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     doas
-    (runCommand "doas-sudo-symlink" {} '' mkdir -pv $out/bin/ && ln -sfv ${pkgs.doas}/bin/doas $out/bin/sudo'')
+    # (runCommand "doas-sudo-symlink" {} '' mkdir -pv $out/bin/ && ln -sfv ${pkgs.doas}/bin/doas $out/bin/sudo'')
   ];
 
   security.doas.enable = true;
