667e864d96aef594a8e5c65b6e79b67732cd8126
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2025-01-24 04:25:18 +0700

    chore(power): enable tlp and configure power profiles (#26)

--- src/systems/power/default.nix
+++ src/systems/power/default.nix
@@ -1,8 +1,17 @@
-{ ... }: {
+{ ... }:
+{
   services.logind.powerKey = "suspend-then-hibernate";
   services.logind.powerKeyLongPress = "poweroff";
   powerManagement = {
     enable = true;
     cpuFreqGovernor = "ondemand";
+  };
+  services.tlp.enable = true;
+  services.tlp.settings = {
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    PLATFORM_PROFILE_ON_BAT = "low-power";
+    CPU_BOOST_ON_BAT = 0;
+    CPU_HWP_DYN_BOOST_ON_BAT = 0;
   };
 }
