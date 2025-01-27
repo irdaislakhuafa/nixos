3108f2a3da98d2c70ae79919eb484714008c6b40
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-01-27 22:19:37 +0700

    feat: use intelephense instead of phpactor

--- home-manager/src/configs/helix/langs/php/default.nix
+++ home-manager/src/configs/helix/langs/php/default.nix
@@ -9,20 +9,25 @@ let
 in
 lib.mkIf (isEnable) {
   home.packages = [
-    # pkgs.intelephense
+    pkgs.intelephense
     pkgs.php
     pkgs.phpactor
+    pkgs.php83Packages.php-codesniffer
   ];
 
   programs.helix.languages = {
     language = [
       {
         name = "php";
-        language-servers = [ "phpactor" ];
+        # language-servers = [         "phpactor"];
         indent = {
           tab-width = 2;
           unit = " ";
         };
+        # formatter = {
+        #   command = "${pkgs.php83Packages.php-codesniffer}/bin/phpcbf";
+        # };
+        auto-format = true;
       }
     ];
 
@@ -32,21 +37,23 @@ lib.mkIf (isEnable) {
       #     args = [ ];
       #     scope = "source.php";
       #   };
-      phpactor = {
-        command = "phpactor";
-        args = [
-          "language-server"
-        ];
-        scope = "source.php";
-      };
+      # phpactor = {
+      #   command = "phpactor";
+      #   args = [
+      #     "language-server"
+      #   ];
+      #   scope = "source.php";
+      # };
     };
   };
 
-  home.file.".config/phpactor/phpactor.json" = {
-    enable = true;
-    text = ''${builtins.toJSON ({
-      "language_server_completion.trim_leading_dollar" = true;
-      "language_server_php_cs_fixer.enabled" = true;
-    })}'';
-  };
+  # home.file.".config/phpactor/phpactor.json" = {
+  #   enable = usePhpActor;
+  #   text = ''${builtins.toJSON ({
+  #     "language_server_completion.trim_leading_dollar" = true;
+  #     "language_server_php_cs_fixer.enabled" = true;
+  #     "php_code_sniffer.enabled" = true;
+  #     "php_code_sniffer.bin" = "${pkgs.php83Packages.php-codesniffer}/bin/phpcbf";
+  #   })}'';
+  # };
 }
