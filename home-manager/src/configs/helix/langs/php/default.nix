d1825811a76f7eeafc2d35934df5205c26d93323
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2025-01-27 21:42:00 +0700

    feat: use phpactor instead of intelephense (#38)

--- home-manager/src/configs/helix/langs/php/default.nix
+++ home-manager/src/configs/helix/langs/php/default.nix
@@ -9,20 +9,16 @@ let
 in
 lib.mkIf (isEnable) {
   home.packages = [
-    pkgs.intelephense
+    # pkgs.intelephense
     pkgs.php
+    pkgs.phpactor
   ];
 
   programs.helix.languages = {
     language = [
       {
         name = "php";
-        roots = [
-          "composer.json"
-          "composer.lock"
-          ".env"
-        ];
-        language-servers = [ "php" ];
+        language-servers = [ "phpactor" ];
         indent = {
           tab-width = 2;
           unit = " ";
@@ -31,11 +27,26 @@ lib.mkIf (isEnable) {
     ];
 
     language-server = {
-      php = {
-        command = "intelephense";
-        args = [ ];
+      #   php = {
+      #     command = "intelephense";
+      #     args = [ ];
+      #     scope = "source.php";
+      #   };
+      phpactor = {
+        command = "phpactor";
+        args = [
+          "language-server"
+        ];
         scope = "source.php";
       };
     };
+  };
+
+  home.file.".config/phpactor/phpactor.json" = {
+    enable = true;
+    text = ''${builtins.toJSON ({
+      "language_server_completion.trim_leading_dollar" = true;
+      "language_server_php_cs_fixer.enabled" = true;
+    })}'';
   };
 }
