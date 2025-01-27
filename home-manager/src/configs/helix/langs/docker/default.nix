074db6785e77ade30059160a5319114fe9593ff8
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-01-27 20:02:54 +0700

    feat: use default config for dockefile and docker compose file

--- home-manager/src/configs/helix/langs/docker/default.nix
+++ home-manager/src/configs/helix/langs/docker/default.nix
@@ -13,38 +13,40 @@ lib.mkIf (isEnable) {
     pkgs.dockerfile-language-server-nodejs
   ];
 
+  # TODO: fix it later, currently better use default config
   programs.helix.languages = {
-    language = [
-      {
-        name = "docker";
-        roots = [
-          "Dockerfile"
-          ".dockerignore"
-          "docker-compose.yaml"
-          "docker-compose.yml"
-        ];
-        language-servers = [
-          "docker"
-          "docker-compose"
-        ];
-        indent = {
-          tab-width = 2;
-          unit = " ";
-        };
-      }
-    ];
+    # language = [
+    #   {
+    #     name = "docker";
+    #     roots = [
+    #       "Dockerfile"
+    #       ".dockerignore"
+    #       "docker-compose.yaml"
+    #       "docker-compose.yml"
+    #     ];
+     
+    #     language-servers = [
+    #       "docker"
+    #     "docker-compose"
+    #     ];
+    #     indent = {
+    #       tab-width = 2;
+    #       unit = " ";
+    #     };
+    #   }
+    # ];
 
-    language-server = {
-      docker = {
-        command = "docker-langserver";
-        args = [ ];
-        scope = "source.dockerfile";
-      };
-      docker-compose = {
-        command = "docker-compose-langserver";
-        args = [ ];
-        scope = "source.docker-compose";
-      };
-    };
+    # language-server = {
+    #   docker = {
+    #     command = "docker-langserver";
+    #     args = [ ];
+    #     scope = "source.dockerfile";
+    #   };
+    #   docker-compose = {
+    #     command = "docker-compose-langserver";
+    #     args = [ ];
+    #     scope = "source.docker-compose";
+    #   };
+    # };
   };
 }
