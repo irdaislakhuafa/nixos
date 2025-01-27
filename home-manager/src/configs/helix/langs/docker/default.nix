34763f51bafbca6b0b2d4a16d6f73a693b4197e0
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-01-27 19:19:40 +0700

    feat: add configuration for dockerfile and docker-compose file

---
+++ home-manager/src/configs/helix/langs/docker/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  isEnable = true;
+in
+lib.mkIf (isEnable) {
+  home.packages = [
+    pkgs.docker-compose-language-service
+    pkgs.dockerfile-language-server-nodejs
+  ];
+
+  programs.helix.languages = {
+    language = [
+      {
+        name = "docker";
+        roots = [
+          "Dockerfile"
+          ".dockerignore"
+          "docker-compose.yaml"
+          "docker-compose.yml"
+        ];
+        language-servers = [
+          "docker"
+          "docker-compose"
+        ];
+        indent = {
+          tab-width = 2;
+          unit = " ";
+        };
+      }
+    ];
+
+    language-server = {
+      docker = {
+        command = "docker-langserver";
+        args = [ ];
+        scope = "source.dockerfile";
+      };
+      docker-compose = {
+        command = "docker-compose-langserver";
+        args = [ ];
+        scope = "source.docker-compose";
+      };
+    };
+  };
+}
