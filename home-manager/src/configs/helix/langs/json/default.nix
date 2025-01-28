bb6ccc53562b793bf467ea260b072d3985ac5d9b
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-01-28 13:54:09 +0700

    feat: add json config

---
+++ home-manager/src/configs/helix/langs/json/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  langs = import ../../langs.nix { };
+  isEnable = langs.json;
+in
+lib.mkIf (isEnable) {
+  home.packages = [
+    pkgs.vscode-langservers-extracted
+  ];
+
+  programs.helix.languages = {
+    language = [
+      {
+        name = "json";
+        language-servers = [ "json" ];
+        indent = {
+          tab-width = 2;
+          unit = " ";
+        };
+      }
+    ];
+
+    language-server = {
+      json = {
+        command = "vscode-json-language-server";
+        args = [ "--stdio" ];
+        config = {
+          provideFormatter = true;
+          keepLines = true;
+          validate = true;
+          format = true;
+        };
+        scope = "source.json";
+      };
+    };
+  };
+}
