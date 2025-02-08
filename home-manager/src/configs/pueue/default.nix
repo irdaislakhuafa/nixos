062f794e2a9c597e8528f9146e5cf9de5da24c2c
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2025-02-09 01:18:43 +0700

    feat: enable pueue and add completions for zsh shell (#61)

--- home-manager/src/configs/pueue/default.nix
+++ home-manager/src/configs/pueue/default.nix
@@ -1,9 +1,28 @@
-{ ... }:
+{ pkgs, config, ... }:
+let
+  isEnable = true;
+  pueueCompletionFile = ".config/pueue/completions.zshrc";
+in
 {
   services.pueue = {
-    enable = false;
+    enable = isEnable;
     settings = {
       default_parallel_tasks = 10;
     };
   };
+  home.file.".config/pueue/completions.zshrc" = {
+    enable = isEnable;
+    source = (
+      pkgs.runCommand "pueue_completions"
+        {
+          buildInputs = [ pkgs.pueue ];
+          shell = pkgs.zsh;
+        }
+        ''
+          ${pkgs.pueue}/bin/pueue completions zsh > $out;
+        ''
+    );
+  };
+
+  programs.zsh.initExtra = ''source ${config.home.homeDirectory}/${pueueCompletionFile}'';
 }
