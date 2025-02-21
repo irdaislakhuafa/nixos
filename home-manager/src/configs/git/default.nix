87e18d55e16cb9539b25acd56c6f40082a5b326f
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-02-22 03:36:12 +0700

    feat: enable delta

--- home-manager/src/configs/git/default.nix
+++ home-manager/src/configs/git/default.nix
@@ -1,12 +1,36 @@
 { pkgs, ... }:
 {
-  home.packages = [
-    # pkgs.gitui
-  ];
+  home.packages = [ ];
 
   programs.git = {
     enable = true;
     userName = "Irda Islakhu Afa";
     userEmail = "irdhaislakhuafa@gmail.com";
+    lfs = {
+      enable = true;
+    };
+    delta = {
+      enable = true;
+      options = {
+        side-by-side = true;
+        line-numbers = true;
+        navigate = true;
+        dark = true;
+      };
+    };
+    extraConfig = {
+      merge = {
+        "hx" =
+          let
+            original = "%O";
+            ancestor = "%A";
+            conflict = "%B";
+          in
+          {
+            name = "Resolve merge conflict with helix";
+            driver = "${pkgs.helix}/bin/hx --vsplit ${original} ${ancestor} ${conflict}";
+          };
+      };
+    };
   };
 }
