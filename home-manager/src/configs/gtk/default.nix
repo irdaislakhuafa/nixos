213dad3198ead530533f260d9b7110aabe6170bc
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-02-26 02:37:53 +0700

    feat: try to use dark theme for gtk

--- home-manager/src/configs/gtk/default.nix
+++ home-manager/src/configs/gtk/default.nix
@@ -1,7 +1,19 @@
 { pkgs, ... }:
 {
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+  };
   gtk = {
     enable = true;
+    theme = {
+      name = "orchis-theme";
+      package = pkgs.orchis-theme;
+    };
     cursorTheme = {
       name = "Adwaita-dark";
       package = pkgs.whitesur-gtk-theme;
