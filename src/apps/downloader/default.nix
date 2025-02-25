ba3ce701f54c400a224c8e3c060d02ed0b85af6d
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-02-25 07:35:13 +0700

    feat: add option for wget2

--- src/apps/downloader/default.nix
+++ src/apps/downloader/default.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   environment.systemPackages = with pkgs; [
     wget2
   ];
-  environment.shellAliases = { wget = "wget2"; };
+  environment.shellAliases = {
+    wget = "wget2 --tls-resume --continue --compression=auto";
+  };
 }
