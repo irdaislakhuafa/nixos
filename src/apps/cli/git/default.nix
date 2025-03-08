d36d408b7abd1927937391021b9b851e2e9c51bc
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2025-03-09 01:16:09 +0700

    ✨ feat: add aliases for git and disks (#77)

--- src/apps/cli/git/default.nix
+++ src/apps/cli/git/default.nix
@@ -1,40 +1,43 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    git
-    gh
-    # gh-dash
-    # gh-cal
-    # gh-eco
-    # lazygit
-    glab
-  ];
-
-  programs.git.enable = true;
-  environment.shellAliases =
+{ pkgs, lib, ... }:
+let
+  aliases = (
     let
-      commandToBranch = { cmd, ... }: ''
-        branch=$(git branch --all | fzf | cut -d ' ' -f 3);
-        if ! [ "$branch" = "" ]; then
-          ${cmd};
-        fi;
-      '';
+      commandToBranch =
+        { cmd, ... }:
+        ''
+          branch=$(git branch --all | fzf | cut -d ' ' -f 3);
+          if ! [ "$branch" = "" ]; then
+            ${cmd};
+          fi;
+        '';
     in
-    rec{
-      gst = ''git status'';
-      gad = ''git add'';
-      gps = ''git push'';
-      grph = ''git log --oneline --decorate --graph --cumulative --pretty'';
-      gout = ''git checkout'';
-      gpl = ''git pull'';
+    lib.mapAttrsToList (name: value: (pkgs.writeShellScriptBin name value)) (rec {
+      gst = ''git status $@'';
+      gad = ''git add $@'';
+      gps = ''git push $@'';
+      grph = ''git log --oneline --decorate --graph --cumulative --pretty $@'';
+      gout = ''git checkout $@'';
+      gpl = ''git pull $@'';
       gouts = commandToBranch { cmd = ''${gout} $branch''; };
-      gbrd = commandToBranch { cmd = ''for b in $(echo $branch | ${pkgs.coreutils-full}/bin/paste -s); do git branch -D $b; done''; };
-      gmit = ''git commit'';
+      gbrd = commandToBranch {
+        cmd = ''for b in $(echo $branch | ${pkgs.coreutils-full}/bin/paste -s); do git branch -D $b; done'';
+      };
+      gmit = ''git commit $@'';
       gam = ''git add --all && ${gmit}'';
       gsdropc = ''git stash && git stash drop'';
-      gsply = ''git stash apply'';
-      gsdrop = ''git stash drop'';
-      gdif = ''git diff'';
-      gfa = ''git fetch --all --prune-tags --prune --jobs=$(cat /sys/devices/cpu/type)'';
-      gbr = ''git branch'';
-    };
+      gsply = ''git stash apply $@'';
+      gsdrop = ''git stash drop $@'';
+      gdif = ''git diff $@'';
+      gfa = ''git fetch --all --prune-tags --prune --jobs=$(cat /sys/devices/cpu/type) $@'';
+      gbr = ''git branch $@'';
+    })
+  );
+in
+{
+  programs.git.enable = true;
+  environment.systemPackages = [
+    pkgs.git
+    pkgs.gh
+    pkgs.glab
+  ] ++ aliases;
 }
