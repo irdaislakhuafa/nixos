af97806e025afdaffd96b7cfe84381087e058304
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-03-09 01:01:48 +0700

    ✨ feat: disable zsh and use fish instead

--- src/apps/shell/fish/default.nix
+++ src/apps/shell/fish/default.nix
@@ -5,5 +5,17 @@ in
 lib.mkIf (isEnable) {
   programs.fish = {
     enable = true;
+    # vendor = {
+    #   config = {
+    #     enable = true;
+    #   };
+    #   completions = {
+    #     enable = true;
+    #   };
+    #   functions = {
+    #     enable = true;
+    #   };
+    # };
+    # useBabelfish = true;
   };
 }
