653d1bbdc49fcaf901e7d3159f96a830df714e6b
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-03-12 01:56:36 +0700

    refactor(docker): comment out docker aliases

--- src/apps/container/docker/default.nix
+++ src/apps/container/docker/default.nix
@@ -1,99 +1,99 @@
 { pkgs, lib, ... }:
 let
   dataRootDir = "/media/Containers/docker";
-  aliases =
-    let
-      delimiter = "|";
-      strFmtImgs = "{{.ID}} ${delimiter} {{.Size}}\t${delimiter} {{.Repository}}:{{.Tag}}";
-      containersActiveCmd =
-        {
-          cmd,
-          args ? "",
-          ...
-        }:
-        ''
-          listID="$(docker ps ${args} | fzf -m | cut -d ' ' -f 1 | paste -s)";
-          ! [ "$(echo $listID)" = "" ] && (${cmd}) 2> /dev/null > /dev/null &! ;
-        '';
-      containersInactiveCmd =
-        {
-          cmd,
-          args ? "",
-          ...
-        }:
-        ''
-          listID="$(docker ps -a ${args} | fzf -m | cut -d ' ' -f 1 | paste -s)";
-          ! [ "$(echo $listID)" = "" ] && (${cmd}) 2> /dev/null > /dev/null &! ;
-        '';
-      imgsCmd =
-        {
-          cmd,
-          args ? "",
-          ...
-        }:
-        ''
-          listID="$(docker images ${args} | fzf -m | cut -d '|' -f 1 | paste -s)";
-          ! [ "$(echo $listID)" = "" ] && (${cmd}) 2> /dev/null > /dev/null &! ;
-        '';
-    in
-    lib.mapAttrsToList (name: value: (pkgs.writeShellScriptBin name value)) ({
-      "doi" = ''
-        docker images $@
-      '';
-      "dops" = ''
-        docker ps $@
-      '';
-      "dopsa" = ''
-        dops -a $@
-      '';
-      "dormiop" = ''
-        ${imgsCmd {
-          cmd = "docker rmi -f $(echo $listID) && notify-send 'Success remove docker images'";
-          args = ''--format="${strFmtImgs}"'';
-        }}
-      '';
-      "doexecit" = ''
-        ${imgsCmd {
-          cmd = "docker rmi -f $(echo $listID) && notify-send 'Success remove docker images'";
-          args = ''--format="${strFmtImgs}"'';
-        }}
-      '';
-      "dormop" = ''
-        ${containersInactiveCmd {
-          cmd = "docker rm $(echo $listID) && notify-send 'Success remove docker containers'";
-        }}
-      '';
-      "dormops" = ''
-        ${containersInactiveCmd {
-          cmd = "docker rm $(echo $listID) && notify-send 'Success remove docker containers'";
-          args = "--size";
-        }}
-      '';
-      "dostart" = ''
-        ${containersInactiveCmd {
-          cmd = ''docker start $(echo $listID) && notify-send "Starting docker container done"'';
-        }}
-      '';
-      "dostop" = ''
-        ${containersActiveCmd {
-          cmd = ''docker stop $(echo $listID) && notify-send "Stoping docker container done"'';
-        }}
-      '';
-      "doctx" = ''
-        ${containersActiveCmd {
-          cmd = ''docker stop $(echo $listID) && notify-send "Stoping docker container done"'';
-        }}
-      '';
-      "docom" = ''
-        docker compose $@
-      '';
-      "dobx" = ''
-        docker buildx $@
-      '';
-      "disbx" = ''
-        distrobox $@
-      '';
-    });
+  # aliases =
+  #   let
+  #     delimiter = "|";
+  #     strFmtImgs = "{{.ID}} ${delimiter} {{.Size}}\t${delimiter} {{.Repository}}:{{.Tag}}";
+  #     containersActiveCmd =
+  #       {
+  #         cmd,
+  #         args ? "",
+  #         ...
+  #       }:
+  #       ''
+  #         listID="$(docker ps ${args} | fzf -m | cut -d ' ' -f 1 | paste -s)";
+  #         ! [ "$(echo $listID)" = "" ] && (${cmd}) 2> /dev/null > /dev/null &! ;
+  #       '';
+  #     containersInactiveCmd =
+  #       {
+  #         cmd,
+  #         args ? "",
+  #         ...
+  #       }:
+  #       ''
+  #         listID="$(docker ps -a ${args} | fzf -m | cut -d ' ' -f 1 | paste -s)";
+  #         ! [ "$(echo $listID)" = "" ] && (${cmd}) 2> /dev/null > /dev/null &! ;
+  #       '';
+  #     imgsCmd =
+  #       {
+  #         cmd,
+  #         args ? "",
+  #         ...
+  #       }:
+  #       ''
+  #         listID="$(docker images ${args} | fzf -m | cut -d '|' -f 1 | paste -s)";
+  #         ! [ "$(echo $listID)" = "" ] && (${cmd}) 2> /dev/null > /dev/null &! ;
+  #       '';
+  #   in
+  #   lib.mapAttrsToList (name: value: (pkgs.writeShellScriptBin name value)) ({
+  #     "doi" = ''
+  #       docker images $@
+  #     '';
+  #     "dops" = ''
+  #       docker ps $@
+  #     '';
+  #     "dopsa" = ''
+  #       dops -a $@
+  #     '';
+  #     "dormiop" = ''
+  #       ${imgsCmd {
+  #         cmd = "docker rmi -f $(echo $listID) && notify-send 'Success remove docker images'";
+  #         args = ''--format="${strFmtImgs}"'';
+  #       }}
+  #     '';
+  #     "doexecit" = ''
+  #       ${imgsCmd {
+  #         cmd = "docker rmi -f $(echo $listID) && notify-send 'Success remove docker images'";
+  #         args = ''--format="${strFmtImgs}"'';
+  #       }}
+  #     '';
+  #     "dormop" = ''
+  #       ${containersInactiveCmd {
+  #         cmd = "docker rm $(echo $listID) && notify-send 'Success remove docker containers'";
+  #       }}
+  #     '';
+  #     "dormops" = ''
+  #       ${containersInactiveCmd {
+  #         cmd = "docker rm $(echo $listID) && notify-send 'Success remove docker containers'";
+  #         args = "--size";
+  #       }}
+  #     '';
+  #     "dostart" = ''
+  #       ${containersInactiveCmd {
+  #         cmd = ''docker start $(echo $listID) && notify-send "Starting docker container done"'';
+  #       }}
+  #     '';
+  #     "dostop" = ''
+  #       ${containersActiveCmd {
+  #         cmd = ''docker stop $(echo $listID) && notify-send "Stoping docker container done"'';
+  #       }}
+  #     '';
+  #     "doctx" = ''
+  #       ${containersActiveCmd {
+  #         cmd = ''docker stop $(echo $listID) && notify-send "Stoping docker container done"'';
+  #       }}
+  #     '';
+  #     "docom" = ''
+  #       docker compose $@
+  #     '';
+  #     "dobx" = ''
+  #       docker buildx $@
+  #     '';
+  #     "disbx" = ''
+  #       distrobox $@
+  #     '';
+  #   });
   # [
   #   (pkgs.writeShellScriptBin "doi" ''
   #     docker images $@
@@ -155,7 +155,7 @@ in
     pkgs.docker
     pkgs.docker-compose
     pkgs.distrobox
-  ] ++ aliases;
+  ];
 
   # docker
   virtualisation.docker = {
