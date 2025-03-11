43b1a917329b8e3f4ae47467de87de7014912b5b
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-03-12 02:23:05 +0700

    chore(hyprland): enable conditional import of hyprland module

--- home-manager/src/configs/hyprland/default.nix
+++ home-manager/src/configs/hyprland/default.nix
@@ -1,10 +1,16 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
+  isEnable = true;
   settings = import ./settings.nix { inherit config; };
   hyprlandAutoStart = import ./configs/modules/autostart.nix { inherit config pkgs; };
   hyprlandConfig = (import ./configs/default.nix { }) + hyprlandAutoStart;
 in
-{
+lib.mkIf (isEnable) {
   imports = [
     ./services/default.nix
     ./hyprlock.nix
