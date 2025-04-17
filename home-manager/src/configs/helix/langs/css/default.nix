b89a4e0bacb5846259dc7585015071432355c2d5
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2025-04-18 01:53:08 +0700

    feat: add support for tailwindcss

--- home-manager/src/configs/helix/langs/css/default.nix
+++ home-manager/src/configs/helix/langs/css/default.nix
@@ -7,17 +7,27 @@
 let
   langs = import ../../langs.nix { };
   isEnable = langs.css;
+  features = {
+    tailwind = {
+      enable = true;
+      pkgs = [ pkgs.tailwindcss-language-server ];
+    };
+  };
 in
 lib.mkIf (isEnable) {
   home.packages = [
     pkgs.vscode-langservers-extracted
-  ];
+  ] ++ (if (features.tailwind.enable) then features.tailwind.pkgs else [ ]);
 
   programs.helix.languages = {
     language = [
       {
         name = "css";
-        language-servers = [ "css" "scls" ];
+        language-servers = [
+          "css"
+          "scls"
+          "tailwind"
+        ];
         indent = {
           tab-width = 2;
           unit = " ";
@@ -36,6 +46,11 @@ lib.mkIf (isEnable) {
           format = true;
         };
         scope = "source.css";
+      };
+      tailwind = {
+        command = "tailwindcss-language-server";
+        args = [ "--stdio" ];
+        config = {};
       };
     };
   };
