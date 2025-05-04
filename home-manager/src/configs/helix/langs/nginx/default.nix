be5e68af979aa964b234cfc6f94c309a4af66a38
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2025-05-04 18:10:36 +0700

    feat(helix): add nginx language support

---
+++ home-manager/src/configs/helix/langs/nginx/default.nix
@@ -0,0 +1,29 @@
+{ lib, ... }:
+let
+  langs = import ../../settings.nix { };
+  isEnable = langs.nginx;
+in
+lib.mkIf (isEnable) {
+  programs.helix.languages = {
+    language = [
+      {
+        name = "nginx";
+        scope = "source.nginx";
+        injection-regex = "nginx";
+        file-types = [
+          { glob = "sites-available/*.conf"; }
+          { glob = "sites-enabled/*.conf"; }
+          { glob = "nginx.conf"; }
+          { glob = "conf.d/*.conf"; }
+        ];
+        roots = [ "nginx.conf" ];
+        comment-token = "#";
+        indent = {
+          tab-width = 4;
+          unit = "    ";
+        };
+        language-servers = ["scls"];
+      }
+    ];
+  };
+}
