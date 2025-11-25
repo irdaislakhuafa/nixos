c7ec4bdd22e6a85506f063d22890ef20699137d5
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2025-11-25 08:12:40 +0700

    feat(helix): truncate git blame output for C-ret
    Pipes git blame output through 'cut' for a more concise display.

--- home-manager/src/configs/helix/keys.nix
+++ home-manager/src/configs/helix/keys.nix
@@ -10,7 +10,7 @@
     A-z = ":toggle-option soft-wrap.enable";
     C-q = ":buffer-close";
     A-ret = [ ":run-shell-command basename $(pwd)" ];
-    C-ret = [ ":run-shell-command echo \"%sh{git blame -L %{cursor_line},+1 %{buffer_name}}\"" ];
+    C-ret = [ ":run-shell-command echo \"%sh{git blame -L %{cursor_line},+1 %{buffer_name} | cut -d' ' -f1-8}\"" ];
   };
   select = { };
 }
