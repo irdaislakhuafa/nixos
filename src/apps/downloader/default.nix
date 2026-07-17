7aa7b03dfd5cc60d92a51dc16df05d76d09f44b2
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2026-07-17 16:37:58 +0700

    feat(downloader): add aria2 and alias for parallel downloads (#113)
    Integrates aria2 for high-speed, parallel downloading.
    Adds a convenient 'aria' shell alias with optimized settings.
    Also comments out unused configuration variables in the Makefile.

--- src/apps/downloader/default.nix
+++ src/apps/downloader/default.nix
@@ -2,8 +2,18 @@
 {
   environment.systemPackages = with pkgs; [
     wget2
+    aria2
   ];
   environment.shellAliases = {
     wget = "wget2 --tls-resume --continue --compression=auto";
+    aria = ''
+      aria2c \
+        -x 16 \
+        -s 16 \
+        -k 1M \
+        --min-split-size=1M \
+        --max-connection-per-server=16 \
+        --optimize-concurrent-downloads=true 
+    '';
   };
 }
