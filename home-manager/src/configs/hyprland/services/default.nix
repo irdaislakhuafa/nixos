0f9bf5c42cca1d490d6d77368ca883ddb13217dd
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2026-08-12 00:55:19 +0700

    refactor(hyprland): use explicit wallpaper path setting

--- home-manager/src/configs/hyprland/services/default.nix
+++ home-manager/src/configs/hyprland/services/default.nix
@@ -17,7 +17,7 @@ in
       WantedBy = [ "graphical-session.target" ];
     };
     Service = {
-      ExecStart = "${pkgs.swaybg}/bin/swaybg --mode fill -i ${settings.dir}/wallpaper.png";
+      ExecStart = "${pkgs.swaybg}/bin/swaybg --mode fill -i ${settings.wallpaper.path}";
       Restart = "on-failure";
     };
   };
