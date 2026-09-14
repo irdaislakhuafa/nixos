22daa24b97b082c948c4e8920e84a246856fa13c
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2026-09-15 04:54:11 +0700

    feat(waybar): add glassy_night_minimal and one_ui themes (#127)
    Adds two new Waybar themes: a minimal glassy night theme and a Samsung
    One UI-inspired floating cards theme. Sets the minimal glassy night
    theme as active and fixes empty window styling in existing themes.

--- home-manager/src/configs/waybar/styles.nix
+++ home-manager/src/configs/waybar/styles.nix
@@ -6,4 +6,7 @@ in
   default = "${dir}/default.css";
   glassy_light = "${dir}/glassy_light.css";
   glassy_night = "${dir}/glassy_night.css";
+  glassy_night_minimal = "${dir}/glassy_night_minimal.css";
+  one_ui = "${dir}/one_ui.css";
+  samsung_oneui = "${dir}/one_ui.css";
 }
